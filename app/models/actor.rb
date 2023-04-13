205a062018d2f87238748e04e575d75967450331
ryhorowitz <ryan.w.horowitz@gmail.com>
2023-04-12 22:25:20 -0400

    commit tree d3805b41f510a75de901e510fb84f20e7f60a29d

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,3 +1,14 @@
 class Actor < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :shows, through: :characters
+
+  def full_name
+    self.first_name + " " + self.last_name
+  end
+
+  def list_roles
+    role = self.characters[0].name
+    show = self.shows[0].name
+    role + " - " + show
+  end
 end
--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,10 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  # has_many :actors, through: :characters
+  belongs_to :network
+
+  def actors_list
+    actor = self.characters[0].actor
+    actor.first_name + " " + actor.last_name
+  end
 end
